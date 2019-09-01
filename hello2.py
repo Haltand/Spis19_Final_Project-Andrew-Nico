7dcda8e343c936487b0657e9a8b68e08dbe556be
Haltand <ashu@ucsd.edu>
2019-08-31 22:21:35 -0700

    Deleted unnecessary files. Change to add API correctly

--- hello2.py
+++ hello2.py
@@ -13,17 +13,9 @@ reddit = praw.Reddit(client_id='Q9UV2ZsbXF0K3w',
 def render_main():
     return render_template('home.html')
 
-@app.route('/ctof')
-def render_ctof():
-    return render_template('ctof.html')
-
 @app.route('/ftoc')
 def render_ftoc():
     return render_template('ftoc.html')
-
-@app.route('/mtokm')
-def render_mtokm():
-    return render_template('mtokm.html')
 
 @app.route('/testit')
 def render_testit():
@@ -42,38 +34,18 @@ def render_ftoc_result():
     except ValueError:
         return "Sorry: something went wrong."
 
-@app.route('/ctof_result')
-def render_ctof_result():
-    try:
-        ctemp_result = float(request.args['cTemp'])
-        ftemp_result = ctof(ctemp_result)
-        return render_template('ctof_result.html', cTemp=ctemp_result, fTemp=ftemp_result)
-    except ValueError:
-        return "Sorry: something went wrong."
-
-@app.route('/mtokm_result')
-def render_mtokm_result():
-    try:
-        miledist_result = float(request.args['miledist'])
-        kilodist_result = miletokm(miledist_result)
-        return render_template('mtokm_result.html', miledist=miledist_result, kilodist=kilodist_result )
-    except ValueError:
-        return "Sorry: something went wrong."
-
-@app.route('/testit')
+@app.route('/testit_result')
 def checksubredtitles():
-    for submission in reddit.subreddit('ucsd').hot(limit=10):
-        print(submission.url)
-        print('\n')
+    for submission in reddit.subreddit('ucsd').hot(limit=1):
+        firstprint = print(submission.title)
+        return render_template('testit_result.html', testresult = firstprint)
+
 
 def ftoc(ftemp):
    return (ftemp-32.0)*(5.0/9.0)
     
 def ctof(ctemp):
    return (ctemp*(9.0/5.0) + 32.0) # replace with correct formula
-
-def miletokm(miledist):
-    return 1.609*miledist
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
