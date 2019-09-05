a7991f6739bd09d169615a3600f3c366ebf6ee16
Haltand <ashu@ucsd.edu>
2019-09-05 13:29:00 -0700

    Fixed grammar

--- hello2.py
+++ hello2.py
@@ -114,7 +114,7 @@ def get_karma(access_token):
     me_json = str(response.json())
     urlsearch = re.compile(r'[w][w][w][.][r][e][d][d][i][t][.][c][o][m][/][r][/][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_][a-z|0-9|/|_]')
     urlresults = urlsearch.search(me_json)
-    return ('The urls are: ' + urlresults.group())
+    return ('The url is: ' + urlresults.group())
 
 
 
